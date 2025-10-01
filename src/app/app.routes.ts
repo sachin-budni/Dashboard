ef5795b2bd5f1cf975781702f42b48b848c06187
sachin-budni <33023021+sachin-budni@users.noreply.github.com>
2025-10-01 12:02:17 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () =>import('./dashboard/dashboard.component').then(c => c.DashboardComponent)  },
+    { path: '**', pathMatch: 'full', redirectTo: ''  }
+];
